65d36d06c0d7b8b32de0ffa88116e2894f2e58ac
Denis <Deni-SelischeV23@yandex.ru>
2023-02-17 00:02:26 +0300

    Solved 4 task

---
+++ Task004/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 1;
+Console.Write("Number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+for (int c = a; c <= b; c++)
+{
+    if (c % 2 == 0)
+    {
+        Console.Write(c + " ");
+    }
+}
+Console.ReadLine();
